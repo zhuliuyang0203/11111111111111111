9fd116f591e1ed88d6132a902b1b1a33512619e7
Diego Molina <diemol@gmail.com>
2025-01-20 20:30:25 +0100

    updating py API docs

--- docs/api/py/_static/documentation_options.js
+++ docs/api/py/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.25.0',
+    VERSION: '4.29.0.202501201850',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
