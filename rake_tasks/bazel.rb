4bec4e0613bcf43de6886adb84603f599cebdd50
titusfortner <titus.fortner@gmail.com>
2025-03-16 18:01:23 -0700

    [build] update ruby bazel module to execute rake tasks with logs

--- rake_tasks/bazel.rb
+++ rake_tasks/bazel.rb
@@ -7,9 +7,7 @@ require 'io/wait'
 require_relative 'selenium_rake/checks'
 
 module Bazel
-  def self.execute(kind, args, target, &block)
-    verbose = Rake::FileUtilsExt.verbose_flag
-
+  def self.execute(kind, args, target, verbose: Rake::FileUtilsExt.verbose_flag, log_file: nil, &block)
     if target.end_with?(':run')
       kind = 'run'
       target = target[0, target.length - 4]
@@ -18,28 +16,34 @@ module Bazel
     cmd = %w[bazel] + [kind, target] + (args || [])
     cmd_out = ''
     cmd_exit_code = 0
+    puts "Executing:\n#{cmd.join(' ')}" if verbose
 
     if SeleniumRake::Checks.windows?
-      cmd += ['2>&1']
       cmd_line = cmd.join(' ')
-      cmd_out = `#{cmd_line}`.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
-      puts cmd_out if verbose
-      cmd_exit_code = $CHILD_STATUS
+      begin
+        cmd_out = `#{cmd_line} 2>&1`.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+        puts cmd_out if verbose
+        File.write(log_file, cmd_out) if log_file
+        cmd_exit_code = $CHILD_STATUS.exitstatus
+      rescue => e
+        raise "Windows command execution failed: #{e.message}"
+      end
     else
-      Open3.popen2e(*cmd) do |stdin, stdouts, wait|
-        is_running = true
-        stdin.close
-        while is_running
-          begin
-            stdouts.wait_readable
-            line = stdouts.readpartial(512)
+      begin
+        Open3.popen2e(*cmd) do |stdin, stdouts, wait|
+          stdin.close
+          log = log_file ? File.open(log_file, 'a') : nil
+          while (line = stdouts.gets)
             cmd_out += line
             $stdout.print line if verbose
-          rescue EOFError
-            is_running = false
+            log&.write(line)
+            log&.flush
           end
+          log&.close
+          cmd_exit_code = wait.value.exitstatus
         end
-        cmd_exit_code = wait.value.exitstatus
+      rescue => e
+        raise "Command execution failed: #{e.message}"
       end
     end
 
