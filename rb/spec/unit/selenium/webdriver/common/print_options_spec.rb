f376af37cddbcdb3f8bb2a92829cdcf24fcc1360
yvsvarma <yvsvarma@gmail.com>
2025-03-31 09:34:04 -0500

    adding copyright text in print_options_spec.rb

--- rb/spec/unit/selenium/webdriver/common/print_options_spec.rb
+++ rb/spec/unit/selenium/webdriver/common/print_options_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# <copyright file="print_options_spec.rb" company="Selenium Committers">
 # Licensed to the Software Freedom Conservancy (SFC) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
