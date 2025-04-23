6b1f51f87089282150e7bba1d517de97ac928c78
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-04-22 21:05:18 -0400

    [py] Fix linting error

--- py/conftest.py
+++ py/conftest.py
@@ -28,7 +28,6 @@ from selenium import webdriver
 from test.selenium.webdriver.common.network import get_lan_ip
 from test.selenium.webdriver.common.webserver import SimpleWebServer
 
-
 drivers = (
     "chrome",
     "edge",
