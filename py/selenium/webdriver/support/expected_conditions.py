9aa794ff4f9336bbc4a7ad0df19cfba6a20411b0
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-05-15 23:16:04 -0400

    [py] Fix docstring

--- py/selenium/webdriver/support/expected_conditions.py
+++ py/selenium/webdriver/support/expected_conditions.py
@@ -388,8 +388,8 @@ def text_to_be_present_in_element(locator: Tuple[str, str], text_: str) -> Calla
     >>> from selenium.webdriver.common.by import By
     >>> from selenium.webdriver.support.ui import WebDriverWait
     >>> from selenium.webdriver.support import expected_conditions as EC
-    >>> is_text_in_element = WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(
-            (By.CLASS_NAME, "foo"), "bar")
+    >>> is_text_in_element = WebDriverWait(driver, 10).until(
+            EC.text_to_be_present_in_element((By.CLASS_NAME, "foo"), "bar")
         )
     """
 
