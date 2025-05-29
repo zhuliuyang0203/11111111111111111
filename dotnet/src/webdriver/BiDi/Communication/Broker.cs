94c736779557186d833ade1d900a312ba0bf5ce8
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2025-05-29 23:00:16 +0300

    [dotnet] [bidi] Simplify modules namespace for end users (breaking change)

--- dotnet/src/webdriver/BiDi/Communication/Broker.cs
+++ dotnet/src/webdriver/BiDi/Communication/Broker.cs
@@ -34,7 +34,7 @@ namespace OpenQA.Selenium.BiDi.Communication;
 
 public sealed class Broker : IAsyncDisposable
 {
-    private readonly ILogger _logger = Log.GetLogger<Broker>();
+    private readonly ILogger _logger = Internal.Logging.Log.GetLogger<Broker>();
 
     private readonly BiDi _bidi;
     private readonly ITransport _transport;
@@ -273,7 +273,7 @@ public sealed class Broker : IAsyncDisposable
         }
     }
 
-    public async Task UnsubscribeAsync(Modules.Session.Subscription subscription, EventHandler eventHandler)
+    public async Task UnsubscribeAsync(Session.Subscription subscription, EventHandler eventHandler)
     {
         var eventHandlers = _eventHandlers[eventHandler.EventName];
 
