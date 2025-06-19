24e5e23080ffa04e8bb059661795e4a036afde57
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2025-06-20 06:33:55 +0700

    [grid] Add config `blocked-routes` and specific `blocked-delete-session` in Router
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- java/src/org/openqa/selenium/grid/router/httpd/RouterServer.java
+++ java/src/org/openqa/selenium/grid/router/httpd/RouterServer.java
@@ -37,6 +37,7 @@ import java.io.UncheckedIOException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -171,6 +172,13 @@ public class RouterServer extends TemplateGridServerCommand {
     if (uap != null) {
       LOG.info("Requiring authentication to connect");
       route = route.with(new BasicAuthenticationFilter(uap.username(), uap.password()));
+    }
+
+    // Apply blocked routes filter
+    List<BlockedRoute> blockedRoutes = routerOptions.getBlockedRoutes();
+    if (!blockedRoutes.isEmpty()) {
+      LOG.info("Blocking " + blockedRoutes.size() + " route(s): " + blockedRoutes);
+      route = BlockedRoutesFilter.with(route, blockedRoutes);
     }
 
     HttpHandler readinessCheck =
