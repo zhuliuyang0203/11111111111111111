6fab3bb23abf434aa4047f8e424fc8dbda766dd3
Diego Molina <diemol@gmail.com>
2025-07-07 17:26:02 +0000

    Format script

--- java/src/org/openqa/selenium/PersistentCapabilities.java
+++ java/src/org/openqa/selenium/PersistentCapabilities.java
@@ -19,7 +19,6 @@ package org.openqa.selenium;
 
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
